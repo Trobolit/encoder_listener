5ef12c8f6cb9954d97c881a00feb16daabf16124
Robert Hedman <robert.hedman@mac.com>
2018-09-27 16:00:55 +0200

    working serial communication

--- encoder_listener.ino
+++ encoder_listener.ino
@@ -4,16 +4,22 @@ const byte interruptPinLd = 4; //direction
 const byte interruptPinRs = 5; //speed
 const byte interruptPinRd = 6; //direction
 volatile long L_count = 0;
-volatile long L_dir = 0;
+volatile byte L_dir = 0;
 volatile long R_count = 0;
-volatile long R_dir = 0;
+volatile byte R_dir = 0;
 long Lc=0;
-long Ld=0;
+byte Ld=0;
 long Rc=0;
-long Rd=0;
+byte Rd=0;
 
+const int MESSAGE_LENGTH = 12; // including SOM and EOMs
+const int EMPTY_MESSAGE = 0;
 
-const long timeThresh = 100; //ms
+// pointer to memory of long so we can send the four bytes individually over serial.
+byte *L_cPtr = (byte*) &Lc;
+byte *R_cPtr = (byte*) &Rc;
+
+const long timeThresh = 50; //ms
 long lastTime = 0;
 
 void setup() {
@@ -41,23 +47,28 @@ void loop() {
     L_dir=0;
     R_count=0;
     R_dir=0;
-    
-    Serial.print("left count: ");
-    Serial.print(Lc);
-    Serial.print("\nleft dir: ");
-    Serial.print(Ld);
-    Serial.print("\nright count ");
-    Serial.print(Rc);   
-    Serial.print("\nright dir: ");
-    Serial.print(Rd);  
-    Serial.print("\n\n");
+
+    Serial.write(250);
+    Serial.write(Ld);
+    for(byte i=3;i<4;i--){ //underflows
+      Serial.write(*(L_cPtr+i));
+    }
+    Serial.write(Rd);
+    for(byte i=3;i<4;i--){ //underflows
+      Serial.write(*(R_cPtr+i));
+    }
+    Serial.write(251);
+
+    // now print zeros, longer than message to make sure reciever cannot get garbage data.
+    for(int i=0;i<MESSAGE_LENGTH+1;i++)
+      Serial.write(EMPTY_MESSAGE);
  
   }
 }
 
 void myInterruptL1() {
     L_count+=1;
-    L_dir = !digitalRead(interruptPinLd); // this checks the phase signal for spinning direction.
+    L_dir = digitalRead(interruptPinLd); // this checks the phase signal for spinning direction.
                                           // Probably not the most elegant solution, but it needs to 
                                           // be done as the count signal rises.
 }
